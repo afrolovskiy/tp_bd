582834e8321bc22a76c32402590554e5946af13d
alex <alex@alex-HP-Pavilion-dv5-Notebook-PC.(none)>
2013-01-19 23:31:38 +0400

    Fix create tables script

--- part1/create__messager_db.sql
+++ part1/create__messager_db.sql
@@ -1,207 +1,174 @@
-
 CREATE TABLE account_contact_list_relationship
 (
 	account_id           INTEGER NOT NULL,
 	contact_list_id      INTEGER NOT NULL,
-	id                   BIGINT NOT NULL
+	id                        BIGINT NOT NULL
 );
-
-
 
 ALTER TABLE account_contact_list_relationship
 ADD PRIMARY KEY (id);
 
-
+ALTER TABLE account_contact_list_relationship
+MODIFY COLUMN id INTEGER AUTO_INCREMENT;
 
 CREATE TABLE account_conversation_relationship
 (
-	account_id           INTEGER NOT NULL,
+	account_id             INTEGER NOT NULL,
 	conversation_id      BIGINT NOT NULL,
-	id                   BIGINT NOT NULL
+	id                          BIGINT NOT NULL
 );
-
-
 
 ALTER TABLE account_conversation_relationship
 ADD PRIMARY KEY (id);
 
-
+ALTER TABLE account_conversation_relationship
+MODIFY COLUMN id INTEGER AUTO_INCREMENT;
 
 CREATE TABLE account_types
 (
 	name                 VARCHAR(32) NOT NULL,
-	account_type_id      SMALLINT NOT NULL
+	id                      SMALLINT NOT NULL
 );
 
-
+ALTER TABLE account_types
+ADD PRIMARY KEY (id);
 
 ALTER TABLE account_types
-ADD PRIMARY KEY (account_type_id);
-
-
+MODIFY COLUMN id SMALLINT AUTO_INCREMENT;
 
 CREATE TABLE accounts
 (
-	created_at           TIMESTAMP NULL,
-	login                VARCHAR(32) NOT NULL,
-	password             VARCHAR(32) NOT NULL,
-	user_id              INTEGER NOT NULL,
-	last_logined_at      TIMESTAMP NULL,
-	type_account_id      SMALLINT NOT NULL,
-	account_id           INTEGER NOT NULL
+	created_at                TIMESTAMP NULL,
+	login                         VARCHAR(32) NOT NULL,
+	password                  VARCHAR(32) NOT NULL,
+	user_id                      INTEGER NOT NULL,
+	last_logined_at           TIMESTAMP NULL,
+	account_types_id         SMALLINT NOT NULL,
+	id                              INTEGER NOT NULL
 );
 
-
+ALTER TABLE accounts
+ADD PRIMARY KEY (id);
 
 ALTER TABLE accounts
-ADD PRIMARY KEY (account_id);
-
-
+MODIFY COLUMN id INTEGER AUTO_INCREMENT;
 
 CREATE TABLE contact_lists
 (
-	alias                VARCHAR(32) NOT NULL,
-	contact_list_id      INTEGER NOT NULL,
+	alias                    VARCHAR(32) NOT NULL,
+	id                        INTEGER NOT NULL,
 	account_id           INTEGER NOT NULL
 );
 
-
+ALTER TABLE contact_lists
+ADD PRIMARY KEY (id);
 
 ALTER TABLE contact_lists
-ADD PRIMARY KEY (contact_list_id);
-
-
+MODIFY COLUMN id INTEGER AUTO_INCREMENT;
 
 CREATE TABLE conversations
 (
-	created_at           TIMESTAMP NULL,
-	is_open              BOOLEAN NOT NULL,
-	conversation_id      BIGINT NOT NULL
+	created_at         TIMESTAMP NULL,
+	is_open             BOOLEAN NOT NULL,
+	id                      BIGINT NOT NULL
 );
 
-
+ALTER TABLE conversations
+ADD PRIMARY KEY (id);
 
 ALTER TABLE conversations
-ADD PRIMARY KEY (conversation_id);
-
-
+MODIFY COLUMN id BIGINT AUTO_INCREMENT;
 
 CREATE TABLE messages
 (
-	content              TEXT NOT NULL,
-	created_at           TIMESTAMP NULL,
-	is_readed            BOOLEAN NOT NULL,
-	account_id           INTEGER NOT NULL,
-	message_id           BIGINT NOT NULL,
+	content                  TEXT NOT NULL,
+	created_at              TIMESTAMP NULL,
+	is_readed               BOOLEAN NOT NULL,
+	account_id             INTEGER NOT NULL,
+	id                          BIGINT NOT NULL,
 	conversation_id      BIGINT NOT NULL
 );
 
-
+ALTER TABLE messages
+ADD PRIMARY KEY (id);
 
 ALTER TABLE messages
-ADD PRIMARY KEY (message_id);
-
-
+MODIFY COLUMN id INTEGER AUTO_INCREMENT;
 
 CREATE TABLE states
 (
 	name                 VARCHAR(32) NOT NULL,
-	state_id             SMALLINT NOT NULL
+	id                      SMALLINT NOT NULL
 );
 
-
+ALTER TABLE states
+ADD PRIMARY KEY (id);
 
 ALTER TABLE states
-ADD PRIMARY KEY (state_id);
-
-
+MODIFY COLUMN id SMALLINT AUTO_INCREMENT;
 
 CREATE TABLE statuses
 (
-	status_message       VARCHAR(128) NULL,
-	account_id           INTEGER NOT NULL,
-	status_id            BIGINT NOT NULL,
-	changed_at           TIMESTAMP NULL,
-	state_id             SMALLINT NOT NULL
+	status_message     VARCHAR(128) NULL,
+	account_id             INTEGER NOT NULL,
+	id                          BIGINT NOT NULL,
+	changed_at            TIMESTAMP NULL,
+	state_id                 SMALLINT NOT NULL
 );
 
-
+ALTER TABLE statuses
+ADD PRIMARY KEY (id);
 
 ALTER TABLE statuses
-ADD PRIMARY KEY (status_id);
-
-
+MODIFY COLUMN id INTEGER AUTO_INCREMENT;
 
 CREATE TABLE users
 (
-	first_name           VARCHAR(128) NULL,
-	last_name            VARCHAR(128) NULL,
+	first_name          VARCHAR(128) NULL,
+	last_name          VARCHAR(128) NULL,
 	is_male              ENUM('None', 'True', 'False'),
 	birthday             DATE NULL,
 	gender               ENUM('None', 'Male', 'Female'),
-	user_id              INTEGER NOT NULL,
-	login                VARCHAR(32) NOT NULL,
-	password             VARCHAR(32) NOT NULL
+	id                      INTEGER NOT NULL,
+	login                  VARCHAR(32) NOT NULL,
+	password           VARCHAR(32) NOT NULL
 );
 
-
+ALTER TABLE users
+ADD PRIMARY KEY (id);
 
 ALTER TABLE users
-ADD PRIMARY KEY (user_id);
-
-
+MODIFY COLUMN id INTEGER AUTO_INCREMENT;
 
 ALTER TABLE account_contact_list_relationship
-ADD FOREIGN KEY R_29 (account_id) REFERENCES accounts (account_id);
-
-
+ADD FOREIGN KEY R_29 (account_id) REFERENCES accounts (id);
 
 ALTER TABLE account_contact_list_relationship
-ADD FOREIGN KEY R_30 (contact_list_id) REFERENCES contact_lists (contact_list_id);
-
-
+ADD FOREIGN KEY R_30 (contact_list_id) REFERENCES contact_lists (id);
 
 ALTER TABLE account_conversation_relationship
-ADD FOREIGN KEY R_27 (account_id) REFERENCES accounts (account_id);
-
-
+ADD FOREIGN KEY R_27 (account_id) REFERENCES accounts (id);
 
 ALTER TABLE account_conversation_relationship
-ADD FOREIGN KEY R_28 (conversation_id) REFERENCES conversations (conversation_id);
-
-
+ADD FOREIGN KEY R_28 (conversation_id) REFERENCES conversations (id);
 
 ALTER TABLE accounts
-ADD FOREIGN KEY R_15 (user_id) REFERENCES users (user_id);
-
-
+ADD FOREIGN KEY R_15 (user_id) REFERENCES users (id);
 
 ALTER TABLE accounts
-ADD FOREIGN KEY R_6 (type_account_id) REFERENCES account_types (account_type_id);
-
-
+ADD FOREIGN KEY R_6 (account_types_id) REFERENCES account_types (id);
 
 ALTER TABLE contact_lists
-ADD FOREIGN KEY R_19 (account_id) REFERENCES accounts (account_id);
-
-
+ADD FOREIGN KEY R_19 (account_id) REFERENCES accounts (id);
 
 ALTER TABLE messages
-ADD FOREIGN KEY R_12 (account_id) REFERENCES accounts (account_id);
-
-
+ADD FOREIGN KEY R_12 (account_id) REFERENCES accounts (id);
 
 ALTER TABLE messages
-ADD FOREIGN KEY R_11 (conversation_id) REFERENCES conversations (conversation_id);
-
-
+ADD FOREIGN KEY R_11 (conversation_id) REFERENCES conversations (id);
 
 ALTER TABLE statuses
-ADD FOREIGN KEY R_18 (account_id) REFERENCES accounts (account_id);
-
-
+ADD FOREIGN KEY R_18 (account_id) REFERENCES accounts (id);
 
 ALTER TABLE statuses
-ADD FOREIGN KEY R_8 (state_id) REFERENCES states (state_id);
-
-
+ADD FOREIGN KEY R_8 (state_id) REFERENCES states (id);
